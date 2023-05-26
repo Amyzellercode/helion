Amyzellercode/helion

commit da840d37ffd94338341bc9ff7012db49a1ac4dcd

Only convert config placeholders when necessary (#9310)
* Only convert config placeholders when necessary

* don't comment out the test

inject/src/main/java/io/micronaut/context/env/PropertySourcePropertyResolver.java
@@ -32,7 +32,6 @@ import io.micronaut.core.util.EnvironmentProperties;
 import io.micronaut.core.util.StringUtils;
 import io.micronaut.core.value.MapPropertyResolver;
 import io.micronaut.core.value.PropertyResolver;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.helpers.NOPLogger;
@@ -782,7 +781,7 @@ public class PropertySourcePropertyResolver implements PropertyResolver, AutoClo
 
     private Object resolvePlaceHoldersIfNecessary(Object value) {
         if (value instanceof CharSequence) {
-            return propertyPlaceholderResolver.resolveRequiredPlaceholders(value.toString());
+            return propertyPlaceholderResolver.resolveRequiredPlaceholdersObject(value.toString());
         } else if (value instanceof List) {
             List<?> list = (List) value;
             List<?> newList = new ArrayList<>(list);

inject/src/main/java/io/micronaut/context/env/DefaultPropertyPlaceholderResolver.java
@@ -101,6 +101,19 @@ public class DefaultPropertyPlaceholderResolver implements PropertyPlaceholderRe
     @Override
     public String resolveRequiredPlaceholders(String str) throws ConfigurationException {
         List<Segment> segments = buildSegments(str);
+        return resolveRequiredPlaceholdersString(segments);
+    }
+
+    @Override
+    public Object resolveRequiredPlaceholdersObject(String str) throws ConfigurationException {
+        List<Segment> segments = buildSegments(str);
+        if (segments.size() == 1) {
+            return segments.get(0).getValue(Object.class);
+        }
+        return resolveRequiredPlaceholdersString(segments);
+    }
+
+    private static String resolveRequiredPlaceholdersString(List<Segment> segments) {
         StringBuilder value = new StringBuilder();
         for (Segment segment: segments) {
             value.append(segment.getValue(String.class));

inject/src/main/java/io/micronaut/context/env/PropertyPlaceholderResolver.java
@@ -16,8 +16,8 @@
 package io.micronaut.context.env;
 
 import io.micronaut.context.exceptions.ConfigurationException;
-
 import io.micronaut.core.annotation.NonNull;
+
 import java.util.Optional;
 
 /**
@@ -44,14 +44,27 @@ public interface PropertyPlaceholderResolver {
     }
 
     /**
-     * Resolve the placeholders and return an Optional String if it was possible to resolve them.
+     * Resolve the placeholders and return a string if it was possible to resolve them.
      *
      * @param str The placeholder to resolve
-     * @return The optional string or {@link Optional#empty()} if resolution was not possible
+     * @return The resolved string
      * @throws ConfigurationException If the placeholders could not be resolved
      */
     default @NonNull String resolveRequiredPlaceholders(String str) throws ConfigurationException {
         return resolvePlaceholders(str).orElseThrow(() -> new ConfigurationException("Unable to resolve placeholders for property: " + str));
+    }
+
+    /**
+     * Resolve the placeholders in the given string. This behaves like
+     * {@link #resolveRequiredPlaceholders(String)}, except that when the whole input is a
+     * placeholder, the value is not converted to String but returned as-is.
+     *
+     * @param str The placeholder to resolve
+     * @return The resolved object or string
+     * @throws ConfigurationException If the placeholders could not be resolved
+     */
+    default @NonNull Object resolveRequiredPlaceholdersObject(String str) throws ConfigurationException {
+        return resolveRequiredPlaceholders(str);
     }
 
     /**

inject/src/test/groovy/io/micronaut/context/env/PropertySourcePropertyResolverSpec.groovy
@@ -155,7 +155,8 @@ class PropertySourcePropertyResolverSpec extends Specification {
         def values = [
                 'foo.bar': '10',
                 'foo.baz': 20,
-                'bar'    : 30
+                'bar'    : 30,
+                'arr'    : new byte[] {1, 2, 3, 0x80},
         ]
         PropertySourcePropertyResolver resolver = new PropertySourcePropertyResolver(
                 PropertySource.of("test", [(property): value] + values)
@@ -188,6 +189,7 @@ class PropertySourcePropertyResolverSpec extends Specification {
         'my.property' | '${FOO_BAR_1}'                                       | 'my.property' | String  | 'foo bar 1'
         'my.property' | 'bolt://${NEO4J_HOST:localhost}:${NEO4J_PORT:32781}' | 'my.property' | String  | 'bolt://localhost:32781'
         'my.property' | '${bar}'                                             | 'my.property' | Integer | 30
+        'my.property' | '${arr}'                                             | 'my.property' | byte[]  | new byte[] {1, 2, 3, 0x80}
     }
 
 

commit 00a3a9aff9242ed0b5e7371fe084e267c912d480

Fix property caching (#9313)
This fixes the build broken by #9310. Basically:

https://github.com/micronaut-projects/micronaut-core/blob/94f458ac04b0180a989d0cf28984d5e3b5df592f/http-client/src/test/groovy/io/micronaut/http/client/filter/StaticClientFilterSpec.groovy#L24-L28

The resolution of micronaut.server.port used to request the value of test-port as String. With this patch, it requests the value as Object instead (`.getValue(Object.class)`). The caching in PropertySourcePropertyResolver depends on the requested type however, so the Object request (micronaut.server.port) and the String request (micronaut.http.services.a.url) are cached separately and have different computed ports. (Additionally Object request type is not cached at all, but this is not the core issue.)

This patch adds another cache *before* conversion that is independent of the requested type. This cache ensures that the value is not recomputed for different request types.

inject/src/main/java/io/micronaut/context/env/PropertySourcePropertyResolver.java
@@ -84,14 +84,22 @@ public class PropertySourcePropertyResolver implements PropertyResolver, AutoClo
     protected Logger log;
 
     private final Map<String, Boolean> containsCache = new ConcurrentHashMap<>(20);
-    private final Map<String, Object> resolvedValueCache = new ConcurrentHashMap<>(20);
+    /**
+     * Cache for values <i>before</i> conversion. This avoids recomputing placeholders, which keeps
+     * random values (e.g. {@code ${random.port}} stable).
+     */
+    private final Map<String, Object> placeholderResolutionCache = new ConcurrentHashMap<>(20);
+    /**
+     * Cache for values <i>after</i> conversion.
+     */
+    private final Map<ConversionCacheKey, Object> resolvedValueCache = new ConcurrentHashMap<>(20);
     private final EnvironmentProperties environmentProperties = EnvironmentProperties.fork(CURRENT_ENV);
 
     /**
      * Creates a new, initially empty, {@link PropertySourcePropertyResolver} for the given {@link ConversionService}.
      *
      * @param conversionService The {@link ConversionService}
-     * @param logEnabled logEnabled flag to enable or disable logger
+     * @param logEnabled        flag to enable or disable logger
      */
     public PropertySourcePropertyResolver(ConversionService conversionService, boolean logEnabled) {
         this.log = logEnabled ? LoggerFactory.getLogger(getClass()) : NOPLogger.NOP_LOGGER;
@@ -319,16 +327,23 @@ public class PropertySourcePropertyResolver implements PropertyResolver, AutoClo
             Objects.requireNonNull(conversionContext, "Conversion context should not be null");
             Class<T> requiredType = conversionContext.getArgument().getType();
             boolean cacheableType = ClassUtils.isJavaLangType(requiredType);
-            Object cached = cacheableType ? resolvedValueCache.get(cacheKey(name, requiredType)) : null;
+            Object cached = cacheableType ? resolvedValueCache.get(new ConversionCacheKey(name, requiredType)) : null;
             if (cached != null) {
                 return cached == NO_VALUE ? Optional.empty() : Optional.of((T) cached);
             } else {
-                Map<String, Object> entries = resolveEntriesForKey(name, false, PropertyCatalog.GENERATED);
-                if (entries == null) {
-                    entries = resolveEntriesForKey(name, false, PropertyCatalog.RAW);
+                Object value = placeholderResolutionCache.get(name);
+                // entries map to get the value from, only populated if there's a cache miss with placeholderResolutionCache
+                Map<String, Object> entries = null;
+                if (value == null) {
+                    entries = resolveEntriesForKey(name, false, PropertyCatalog.GENERATED);
+                    if (entries == null) {
+                        entries = resolveEntriesForKey(name, false, PropertyCatalog.RAW);
+                    }
                 }
-                if (entries != null) {
-                    Object value = entries.get(name);
+                if (entries != null || value != null) {
+                    if (value == null) {
+                        value = entries.get(name);
+                    }
                     if (value == null) {
                         value = entries.get(normalizeName(name));
                         if (value == null && name.indexOf('[') == -1) {
@@ -373,7 +388,11 @@ public class PropertySourcePropertyResolver implements PropertyResolver, AutoClo
 
                     if (value != null) {
                         Optional<T> converted;
-                        value = resolvePlaceHoldersIfNecessary(value);
+                        if (entries != null) {
+                            // iff entries is null, the value is from placeholderResolutionCache and doesn't need this step
+                            value = resolvePlaceHoldersIfNecessary(value);
+                            placeholderResolutionCache.put(name, value);
+                        }
                         if (requiredType.isInstance(value) && !CollectionUtils.isIterableOrMap(requiredType)) {
                             converted = (Optional<T>) Optional.of(value);
                         } else {
@@ -389,11 +408,11 @@ public class PropertySourcePropertyResolver implements PropertyResolver, AutoClo
                         }
 
                         if (cacheableType) {
-                            resolvedValueCache.put(cacheKey(name, requiredType), converted.orElse((T) NO_VALUE));
+                            resolvedValueCache.put(new ConversionCacheKey(name, requiredType), converted.orElse((T) NO_VALUE));
                         }
                         return converted;
                     } else if (cacheableType) {
-                        resolvedValueCache.put(cacheKey(name, requiredType), NO_VALUE);
+                        resolvedValueCache.put(new ConversionCacheKey(name, requiredType), NO_VALUE);
                         return Optional.empty();
                     } else if (Properties.class.isAssignableFrom(requiredType)) {
                         Properties properties = resolveSubProperties(name, entries, conversionContext);
@@ -424,15 +443,10 @@ public class PropertySourcePropertyResolver implements PropertyResolver, AutoClo
         return Optional.empty();
     }
 
-    @NonNull
-    private <T> String cacheKey(@NonNull String name, Class<T> requiredType) {
-        return name + '|' + requiredType.getSimpleName();
-    }
-
     /**
      * Returns a combined Map of all properties in the catalog.
      *
-     * @param keyConvention The map key convention
+     * @param keyConvention  The map key convention
      * @param transformation The map format
      * @return Map of all properties
      */
@@ -749,6 +763,7 @@ public class PropertySourcePropertyResolver implements PropertyResolver, AutoClo
     protected void resetCaches() {
         containsCache.clear();
         resolvedValueCache.clear();
+        placeholderResolutionCache.clear();
     }
 
     private void processSubmapKey(Map<String, Object> map, String key, Object value, @Nullable StringConvention keyConvention) {
@@ -851,5 +866,8 @@ public class PropertySourcePropertyResolver implements PropertyResolver, AutoClo
          * {@code foo.bar-baz}, and {@code foo-bar.baz}.
          */
         GENERATED
+    }
+
+    private record ConversionCacheKey(@NonNull String name, Class<?> requiredType) {
     }
 }