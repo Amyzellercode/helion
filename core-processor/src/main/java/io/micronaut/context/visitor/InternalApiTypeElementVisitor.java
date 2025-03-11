micronaut-projects/micronaut-core

commit 51e35b16bbf0074e2741a250578f557b7621ae1b

Allow suppressing internal warnings (#10584)
* Allow suppressing internal warnings

This PR allows suppressing internal/experimental warnings. In an internal project, this output accounts for ~12% of the whole log, and when you know what you're doing, it's unnecessary.

* Fix Checkstyle

* Use dots

core-processor/src/main/java/io/micronaut/context/visitor/InternalApiTypeElementVisitor.java
@@ -18,6 +18,7 @@ package io.micronaut.context.visitor;
 import io.micronaut.core.annotation.Experimental;
 import io.micronaut.core.annotation.Internal;
 import io.micronaut.core.annotation.NonNull;
+import io.micronaut.core.util.StringUtils;
 import io.micronaut.inject.ast.ClassElement;
 import io.micronaut.inject.ast.ConstructorElement;
 import io.micronaut.inject.ast.Element;
@@ -38,6 +39,8 @@ import io.micronaut.inject.visitor.VisitorContext;
 public class InternalApiTypeElementVisitor implements TypeElementVisitor<Object, Object> {
 
     private static final String IO_MICRONAUT = "io.micronaut";
+
+    private static final String MICRONAUT_PROCESSING_INTERNAL_WARNINGS = "micronaut.processing.internal.warnings";
 
     private boolean warned = false;
 
@@ -78,14 +81,21 @@ public class InternalApiTypeElementVisitor implements TypeElementVisitor<Object,
     private void warn(Element element, VisitorContext context) {
         if (element.hasAnnotation(Internal.class) || element.hasAnnotation(Experimental.class)) {
             warned = true;
-            context.warn("Element extends or implements an internal or experimental Micronaut API", element);
+            if (warnEnabled(context)) {
+                context.warn("Element extends or implements an internal or experimental Micronaut API", element);
+            }
         }
     }
 
     @Override
     public void finish(VisitorContext visitorContext) {
-        if (warned) {
+        if (warned && warnEnabled(visitorContext)) {
             visitorContext.warn("Overriding an internal Micronaut API may result in breaking changes in minor or patch versions of the framework. Proceed with caution!", null);
         }
+    }
+
+    private boolean warnEnabled(VisitorContext visitorContext) {
+        String value = visitorContext.getOptions().get(MICRONAUT_PROCESSING_INTERNAL_WARNINGS);
+        return value == null || StringUtils.TRUE.equals(value);
     }
 }

core/src/main/java/io/micronaut/core/annotation/AnnotationValue.java
@@ -21,7 +21,6 @@ import io.micronaut.core.convert.ConversionService;
 import io.micronaut.core.convert.value.ConvertibleValues;
 import io.micronaut.core.expressions.EvaluatedExpression;
 import io.micronaut.core.reflect.ClassUtils;
-import io.micronaut.core.reflect.ReflectionUtils;
 import io.micronaut.core.type.Argument;
 import io.micronaut.core.util.ArgumentUtils;
 import io.micronaut.core.util.ArrayUtils;

core/src/main/java/io/micronaut/core/annotation/Experimental.java
@@ -19,7 +19,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 /**
- * Annotates a class or method as being experimental and subject to change or removal.
+ * <p>Annotates a class or method as being experimental and subject to change or removal.</p>
+ *
+ * <p>Overriding an experimental element will produce a compilation warning. These warnings can be
+ * suppressed by setting an annotation processing argument:
+ * <code>-Amicronaut.processing.internal.warnings=false</code></p>
  *
  * @author Graeme Rocher
  * @since 1.0

core/src/main/java/io/micronaut/core/annotation/Internal.java
@@ -21,7 +21,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
 /**
- * Annotates a class or method regarded as internal and not for public consumption.
+ * <p>Annotates a class or method regarded as internal and not for public consumption.</p>
+ *
+ * <p>Overriding an internal element will produce a compilation warning. These warnings can be
+ * suppressed by setting an annotation processing argument:
+ * <code>-Amicronaut.processing.internal.warnings=false</code></p>
  *
  * @author Graeme Rocher
  * @since 1.0

Amyzellercode/helion

commit 48d318fc206432f75d02e16e51dadbd806444a8f

fix compiler warning for unrecognised options (#11648)
Currently the following warnings are being printed when compiling with Micronaut

```
warning: The following options were not recognized by any processor: '[micronaut.processing.internal.warnings]'
1 warning

warning: The following options were not recognized by any processor: '[micronaut.processing.internal.warnings]'
```

This regression was introduced by https://github.com/micronaut-projects/micronaut-core/commit/51e35b16bbf0074e2741a250578f557b7621ae1b

Which didn't return `getSupportedOptions()` for the new annotation processor argument.

core-processor/src/main/java/io/micronaut/context/visitor/InternalApiTypeElementVisitor.java
@@ -55,6 +55,11 @@ public class InternalApiTypeElementVisitor implements TypeElementVisitor<Object,
         );
     }
 
+    @Override
+    public Set<String> getSupportedOptions() {
+        return Set.of(MICRONAUT_PROCESSING_INTERNAL_WARNINGS);
+    }
+
     @NonNull
     @Override
     public VisitorKind getVisitorKind() {